85bd54428d1c0dcb9a89397f724b2b80224365d8
ChungCNTT <141484007+ChungCNTT@users.noreply.github.com>
2025-04-08 12:39:27 +0700

    background

--- app/src/main/java/vn/edu/tlu/cse/amourswip/MainActivity.java
+++ app/src/main/java/vn/edu/tlu/cse/amourswip/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.item_message_sent);
     }
 }
