39403005459a2f67e698b79445f1246050f2b9c5
truongcoder1 <danghuutruong790@gmail.com>
2025-04-08 15:00:57 +0700

    background ---

--- app/src/main/java/vn/edu/tlu/cse/amourswip/MainActivity.java
+++ app/src/main/java/vn/edu/tlu/cse/amourswip/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.item_message_sent);
+        setContentView(R.layout.activity_edit_profile);
     }
 }
